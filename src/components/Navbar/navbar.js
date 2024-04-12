47d8251d535ddf96d61850d652004dd5eb5ff934
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-04-12 22:23:41 +0530

    A sligh colour change in Logo & Coontact Button over navbar.

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './navbar.css'
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo2.png';
 import contactImg from '../../assets/contact.png'
 import {Link} from "react-scroll";
 
